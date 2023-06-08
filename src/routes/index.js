3af0db60f9a716059b6bcdd7009ec7275e76c1c5
Daniel Rodriguez <kungary04@gmail.com>
2023-06-07 23:29:12 -0500

    Cart rename route

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,7 +11,7 @@ router.use('/users', userRouter);
 router.use('/categories', categoryRouter);
 router.use('/products', productRouter);
 router.use('/products_images', productImgRouter);
-router.use('/carts', cartRouter);
+router.use('/cart', cartRouter);
 router.use('/purchases', purchaseRouter);
 
 
